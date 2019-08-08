3b2d95e79d911868b9a4141805c4c173b4cfaf3e
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2019-08-08 18:59:16 -0400

    Add next link

--- index.js
+++ index.js
@@ -8,33 +8,18 @@ app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req,res,next)=>{
-    res.json('Hello World');
-});
-
-app.get('/cats/:name', (req,res,next)=>{
-    var name = req.params.name;
-
-    res.render('first', {
-      content: 'Meow',
-      name: name,
-      showHello: 5
-    });
-});
-
 app.get('/dogs', (req, res, next) => {
-  var dogNum = Number(req.query.num) - 1;
+  var queryNum = Number(req.query.num);
+  var dogNum = queryNum - 1;
   var dog = dogData[dogNum];
+  var nextNum = queryNum + 1;
 
   res.render('pets', {
-    name: dog["name"],
-    description: dog["description"],
-    imgURL: dog["imgURL"]
+    name: dog.name,
+    description: dog.description,
+    nextNum: nextNum,
+    imgURL: dog.imgURL
   })
-});
-
-app.get('/cats_and_dogs', (req,res,next) => {
-    res.json('living together');
 });
 
 app.listen(3000, function(){
